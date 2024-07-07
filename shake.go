ad5cd177282ea1c2efef68c95e1d2604ba139049
zacksfF <ssssaasss571@gmail.com>
2024-07-07 14:00:06 +0100

    add shake 👾

--- shake.go
+++ shake.go
@@ -1,1 +1,13 @@
 package keccak256sha3
+
+import "hash"
+
+// NewSHAKE128 returns a new hash.Hash computing SHAKE128 with a n*8 bit output as specified in the FIPS 202 draft.
+func NewSHAKE128(n int) hash.Hash {
+	return NewKeccak(128*2, n*8, domainSHAKE)
+}
+
+// NewSHAKE256 returns a new hash.Hash computing SHAKE256 with a n*8 bit output as specified in the FIPS 202 draft.
+func NewSHAKE256(n int) hash.Hash {
+	return NewKeccak(256*2, n*8, domainSHAKE)
+}
